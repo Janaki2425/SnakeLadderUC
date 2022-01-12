6898008f351de53e0c1e3a8ceb65ea35daff0e68
Janu shankar <janakiharishankar24@gmail.com>
2022-01-12 19:14:23 +0530

    adding an file

---
+++ RollDice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class RollDice
+    {
+        public static void RollDice1to6()
+        {
+            int player = 1;
+
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+            Console.WriteLine("No. of Player in there " + player);
+
+            Console.WriteLine("You got number :" + DiceRoll);
+            Console.ReadLine();
+        }
+    }
+}
