d1adb965d9be990295382830e707850ff547e1c5
Janu shankar <janakiharishankar24@gmail.com>
2022-01-12 19:08:11 +0530

    adding an file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayerPosition Uc1 = new PlayerPosition();
+            PlayerPosition.CheckingPlayerPosition();
+            //    RollDice Uc2 = new RollDice();
+            //    RollDice.RollDice1to6();
+            //    RepeatTill100 Uc3 =new RepeatTill100();
+            //    RepeatTill100.Repeat();
+            //    StartPosition uc5 = new StartedPosition();
+            //    StartPosition.PlayerAtPositionZero();
+            //    DiceRollCount Uc6 = new DiceRollCount();
+            //    DiceRollCount.CheckNoOfTimesDiceRoll();
+        }
+    }
+}
